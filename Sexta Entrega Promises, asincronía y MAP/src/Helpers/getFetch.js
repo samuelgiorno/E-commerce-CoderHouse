70c65172a0405a789f2748710c952ee8a7f1c8f7
samuelgiorno <90120898+samuelgiorno@users.noreply.github.com>
2021-12-19 17:27:26 -0500

    Update getFetch.js

--- Sexta Entrega Promises, asincronía y MAP/src/Helpers/getFetch.js
+++ Sexta Entrega Promises, asincronía y MAP/src/Helpers/getFetch.js
@@ -1,9 +1,9 @@
 const productos = [
-    {id: 1, img: 'http://imgfz.com/i/DreEmIp.png', name: 'Item 1', description: 'Descripción del item 1', price: '10'},
-    {id: 2, img: 'http://imgfz.com/i/DreEmIp.png', name: 'Item 2', description: 'Descripción del item 2', price: '10'},
-    {id: 3, img: 'http://imgfz.com/i/DreEmIp.png', name: 'Item 3', description: 'Descripción del item 3', price: '10'},
-    {id: 4, img: 'http://imgfz.com/i/DreEmIp.png', name: 'Item 4', description: 'Descripción del item 4', price: '10'},
-    {id: 5, img: 'http://imgfz.com/i/DreEmIp.png', name: 'Item 5', description: 'Descripción del item 5', price: '10'},
+    {id: 1, img: 'https://assets.reebok.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/678f99c984894bd89135ac610005fb2a_9366/Zapatillas_Reebok_Vector_Smash_Blanco_FZ0288_01_standard.jpg', name: 'Item 1', description: 'Descripción del item 1', price: '10'},
+    {id: 2, img: 'https://assets.reebok.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/678f99c984894bd89135ac610005fb2a_9366/Zapatillas_Reebok_Vector_Smash_Blanco_FZ0288_01_standard.jpg', name: 'Item 2', description: 'Descripción del item 2', price: '10'},
+    {id: 3, img: 'https://assets.reebok.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/678f99c984894bd89135ac610005fb2a_9366/Zapatillas_Reebok_Vector_Smash_Blanco_FZ0288_01_standard.jpg', name: 'Item 3', description: 'Descripción del item 3', price: '10'},
+    {id: 4, img: 'https://assets.reebok.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/678f99c984894bd89135ac610005fb2a_9366/Zapatillas_Reebok_Vector_Smash_Blanco_FZ0288_01_standard.jpg', name: 'Item 4', description: 'Descripción del item 4', price: '10'},
+    {id: 5, img: 'https://assets.reebok.com/images/h_840,f_auto,q_auto:sensitive,fl_lossy,c_fill,g_auto/678f99c984894bd89135ac610005fb2a_9366/Zapatillas_Reebok_Vector_Smash_Blanco_FZ0288_01_standard.jpg', name: 'Item 5', description: 'Descripción del item 5', price: '10'},
 ];
 
 export const getFetch = new Promise ((resolve) => {
